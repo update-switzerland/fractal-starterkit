2fb626ce9ece0fc490c1222eccf9c4399e287066
Orkun Atasoy <orkunatasoy1996@live.de>
2020-12-01 15:16:48 +0100

    Initial Commit

---
+++ components/01-components/button/button.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $('button').click(function(e) {
+        alert("You have clicked the button");
+    });
+});
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const gulp         = require('gulp');
+const fractal      = require('./fractal.config.js');
+const logger       = fractal.cli.console;
+const less         = require('gulp-less');
+const lessGlob     = require('gulp-less-glob');
+const concat       = require('gulp-concat');
+const rename       = require('gulp-rename');
+const uglify       = require('gulp-uglify');
+const plumber      = require('gulp-plumber');
+const notify       = require('gulp-notify');
+const path         = require('path');
+
+gulp.task('less',function() {
+    return gulp.src('public/css/main.less')
+    .pipe(customPlumber('Error running Less'))
+    .pipe(lessGlob())
+    .pipe(less())
+    .pipe(gulp.dest('public/css'))
+});
+
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'public/vendor/**/*.js',
+        'components/**/*.js',
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch([
+        'components/**/*.less',
+        'public/**/*.less'
+        ], gulp.series('less'));
+    gulp.watch([
+        'public/vendor/**/*.js',
+        'components/**/*.js',
+         ], gulp.series('scripts'));
+});
+
+
+function customPlumber(errTitle) {
+    return plumber({
+        errorHandler: notify.onError({
+            title: errTitle || "Error running Gulp",
+            message: "Error: <%= error.message %>",
+        })
+    });
+}
+
+
+gulp.task('fractal:start', function(){
+    const server = fractal.web.server({
+        sync: true
+    });
+    server.on('error', err => logger.error(err.message));
+    return server.start().then(() => {
+        logger.success(`Fractal server is now running at ${server.url}`);
+    });
+});
+
+
+gulp.task('fractal:build', function(){
+    const builder = fractal.web.builder();
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
+    return builder.build().then(() => {
+        logger.success('Fractal build completed!');
+    });
+});
+
+
+gulp.task('default', gulp.series('fractal:start', 'less', 'scripts', 'watch'));
